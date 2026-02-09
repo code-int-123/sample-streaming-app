c2ce0b74ecca8b38c49064fad5db20094db7da41
william <williamlin59@hotmail.com>
2026-02-09 18:34:34 +0000

    inital commit

---
+++ src/main/java/com/checkout/steaming_app/SteamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.checkout.steaming_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamingAppApplication.class, args);
+	}
+
+}
