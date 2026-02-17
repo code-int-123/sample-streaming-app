32421226affb589d960f7ec01e7ea435205042aa
william <williamlin59@hotmail.com>
2026-02-17 00:10:01 +0000

    add deployment for page_view_sink

--- streaming-app-infra/outputs.tf
+++ streaming-app-infra/outputs.tf
@@ -19,3 +19,23 @@ output "ec2_public_ip" {
   description = "EC2 instance public IP"
   value       = aws_instance.page_view_aggregator.public_ip
 }
+
+output "ecr_repository_url_sink" {
+  description = "ECR repository URL for page-view-sink"
+  value       = aws_ecr_repository.page_view_sink.repository_url
+}
+
+output "s3_sink_bucket" {
+  description = "S3 bucket for page-view-sink output"
+  value       = aws_s3_bucket.page_view_sink.bucket
+}
+
+output "ec2_sink_instance_id" {
+  description = "Page view sink EC2 instance ID"
+  value       = aws_instance.page_view_sink.id
+}
+
+output "ec2_sink_public_ip" {
+  description = "Page view sink EC2 instance public IP"
+  value       = aws_instance.page_view_sink.public_ip
+}
